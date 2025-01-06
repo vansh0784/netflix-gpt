2be58efc59bce129edad912d73bb9f7ebf64a50a
vansh <vanshsin784@gmail.com>
2025-01-06 23:37:06 +0530

    Working on UI Day1

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,39 @@ import React from 'react'
 import Header from './Header'
 const Login = () => {
   return (
-    <div>
-      <img className="absolute"
+    <div className='relative'>
+      <img className="absolute w-full h-auto object-cover "
           src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg"
           alt="bg-cover"
         />
+      <div className="absolute top-0 left-0 w-full h-screen bg-black opacity-75 z-10"></div>
       <Header/>
-      <div className='bg-black px-4 py-4 absolute my-36 mx-auto right-0 left-0 w-3/12'>
-        <h1 className='text-white px-4 py-4 font-bold text-3xl'>Sign In</h1>
+      <div className='bg-black text-white rounded-md bg-opacity-90 px-10 py-4 shadow-xl absolute my-36 text-center mx-auto right-0 left-0 w-3/12 z-10'>
         <form onSubmit={(e)=>{e.preventDefault()}}
-          className='text-center'>
-            <input className='outline-none px-2 py-1 my-4 bg-slate-900'
+          className='text-center text-white'>
+            <h1 className='text-left py-4 font-bold text-3xl my-4'>Sign In</h1>
+            <input className='outline-none px-3 py-3 my-2 mx-auto border opacity-50 rounded-sm bg-slate-900 w-full'
              type="text" placeholder='Email or phone number' />
-            <input className='outline-none px-2 py-1 bg-slate-900' type="password" placeholder='Password' />
-            <button className='bg-red-600 my-4 text-white '>Submit</button>
+            <input className='outline-none px-3 py-3 my-2 mx-auto border opacity-50 rounded-sm bg-slate-900 w-full' type="password" placeholder='Password'/>
+            <button className='bg-red-600 my-3 px-3 rounded-sm py-3 text-center w-full'>Sign In</button>
+            <h3>OR</h3>
+            <button className='bg-gray-600 rounded-sm my-3 px-3 py-3 text-center w-full'>Use a sign-in code</button>
+            <a href="/forgotPassword" className='my-3 px-2 py-1' >Forgot password?</a>
+            <div className="flex items-left mt-4">
+            <input
+              type="checkbox"
+              id="rememberMe"
+              className="mr-2"
+              name="rememberMe"
+            />
+            <label htmlFor="rememberMe" className="text-base">
+              Remember Me
+            </label>
+          </div>
+          <div className='text-left text-white my-3'>
+              <h6 className='text-gray-400 my-3'>New to netflix <a href="/signup" className='text-white font-bold'>Sign up now</a></h6>
+              <p className='text-xs my-2'>This page is protected by Google reCAPTCHA to ensure you're not a bot.<span className='text-blue-700 px-1 text-xs'>Learn more.</span></p>
+          </div>
         </form>
       </div>
       </div>
@@ -23,4 +42,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,21 +1,45 @@
 import React from "react";
-import Header from "./Header"
+import Header from "./Header";
+
 const Signup = () => {
   return (
-    <div>
-      <img className="absolute"
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg"
-          alt="bg-cover"
-        />
-      <Header/>
-      <div className=" absolute h-screen w-full top-1/2 left-1/2 text-center text-white">
-        <h1 className="font-bold ">Unlimited movies,</h1>
-        <h1 className="font-bold">TV shows and more</h1>
-        <h3 className="font-semibold">Starts at ₹149. Cancel at any time.</h3>
+    <div className="relative">
+      <img
+        className="absolute w-full h-auto object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg"
+        alt="bg-cover"
+      />
+      <div className="absolute top-0 left-0 w-full h-screen bg-black opacity-75 z-10"></div>
+      <div className="absolute my-6 left-70 right-80 text-white mx-auto z-20">
+        <select  className="bg-black opacity-40 px-4 py-2 border rounded-md" name="">
+          <option className="" value="English">English</option>
+          <option className="" value="हिन्दी">हिन्दी</option>
+        </select>
+        <button className="bg-red-600 mx-2 px-5 py-2 rounded-md" ><a href="/">Sign In</a></button>
+      </div>
+      <Header />
 
+      <div className="absolute my-36 text-center mx-auto right-0 left-0 text-white z-10">
+        <h1 className="font-bold text-6xl my-4">Unlimited movies,</h1>
+        <h1 className="font-bold text-6xl my-4">TV shows and more</h1>
+        <h3 className="font-semibold text-xl my-4">
+          Starts at ₹149. Cancel at any time.
+        </h3>
+        <h2 className="font-semibold text-xl my-2">
+          Ready to watch? Enter your email to create or restart your membership.
+        </h2>
+        <div className="flex items-center justify-center">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="outline-none px-6 py-3 my-2 bg-slate-900 w-1/4 bg-opacity-80 border rounded-md "
+          ></input>
+          <button className="bg-red-600 px-6 py-3 font-semibold rounded-md my-2 mx-2 text-white">
+            Get Started {">"}
+          </button>
+        </div>
       </div>
     </div>
   );
 };
-
 export default Signup;
